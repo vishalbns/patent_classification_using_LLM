a75c433b39fb16dd54a43ea1169cc12350f19176
Vishal Basutkar <vishalbns@Vishals-MacBook-Pro.local>
2024-12-19 16:23:11 -0500

    serve edit

--- src/serve.py
+++ src/serve.py
@@ -59,4 +59,4 @@ def predict(request: TextRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=9000)
+    uvicorn.run(app, host="34.58.248.101", port=9000)
